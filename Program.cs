24d9f26a600074e9d5a2c78f04edcb36d96dcdb7
habita73 <habita3@hotmail.com>
2026-06-11 22:10:27 +0000

    Initial -2

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using AnalogPrayerTimer.Models;
 
 Console.WriteLine("Wellcome to the Analog Prayer Timer application!");
 Console.WriteLine();
-
-var location = new Location { Latitude = 24.7136, Longitude = 46.6753 };
+//33.40367186308134, 6.822887362985581
+var location = new Location { Latitude = 6.822887362985581, Longitude = 33.40367186308134 };
 var calculator = new PrayerTimeCalculator();
 var today = DateTime.Today;
 var prayerTimes = calculator.CalculateTimes(today, location);
